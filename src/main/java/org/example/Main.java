e6439cf1cc46f8193c4c920925922ce9b82a676e
Dries Van de Velde <dries.vandevelde@student.kuleuven.be>
2024-04-29 09:18:07 +0200

    new branch initialized

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,19 +11,13 @@ public class Main {
 
         Instance instance = new Instance();
         AssignmentMatrix assignmentMatrix = new AssignmentMatrix(instance);
-//        Preprocessing p = new Preprocessing(instance.getOpponents(), 1,3, assignmentMatrix.getTranslationMatrix());
-//        p.preProcessQ1andQ2();
-        //assignmentMatrix.setTranslationMatrix(p.getMatchPairs());
         BranchAndBound branchAndBound = new BranchAndBound(assignmentMatrix);
         branchAndBound.executeBranchAndBound();
 
-        //System.out.println("debug");
         for (int i = 0; i < assignmentMatrix.getnUmpires(); i++){
-            //System.out.println("Umpire " + i);
             int[] homeTeams = new int[instance.getnTeams()];
             for (int j = 0; j < assignmentMatrix.getnRounds(); j++){
                 homeTeams[assignmentMatrix.getSolutionMatrix()[j][i].getHomeTeam()-1]++;
-                //System.out.print(assignmentMatrix.getSolutionMatrix()[j][i] + " ");
             }
             int counter = 0;
             for (int team : homeTeams){
@@ -32,14 +26,10 @@ public class Main {
                 }
             }
             if (counter != instance.getnTeams()){
-                //System.out.println("Not all teams have been assigned to umpire " + i);
             }
-            //System.out.println();
         }
 
         long endTime = System.currentTimeMillis();
-
-
 
         SolutionConverter c = new SolutionConverter(assignmentMatrix.getSolutionMatrix(), assignmentMatrix.getTranslationMatrix());
         c.printSolution(c.convertSolutionMatrixMultipleLines());
